7b9ffb02657beacc52a7cb041d530ed6f2e6851b
Faith Rotich <rotichfaith43@gmail.com>
2024-12-06 17:21:00 +0000

    Solved 1-18

--- src/rich4clojure/elementary/problem_008.clj
+++ src/rich4clojure/elementary/problem_008.clj
@@ -8,15 +8,17 @@
 ;; 
 ;; Sets are collections of unique values.
 
-(def __ :tests-will-fail)
+(def __ (set '(:a :b :c :d)))
 
 (comment
   
   )
 
 (tests
-  __ := (set '(:a :a :b :c :c :c :c :d :d))
-  __ := (clojure.set/union #{:a :b :c} #{:b :c :d}))
+ __ := (set '(:a :a :b :c :c :c :c :d :d))
+ __ := (clojure.set/union #{:a :b :c} #{:b :c :d})
+ )
+
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/d1f6098dff344875902737842e15543f

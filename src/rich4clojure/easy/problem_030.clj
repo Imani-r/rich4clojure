e41f22db84ad3fbfec59f39f5c3ba856570423c8
Faith Rotich <rotichfaith43@gmail.com>
2025-06-14 23:42:22 +0100

    solved 30 - dedupe

--- src/rich4clojure/easy/problem_030.clj
+++ src/rich4clojure/easy/problem_030.clj
@@ -9,16 +9,22 @@
 ;; Write a function which removes consecutive duplicates
 ;; from a sequence.
 
-(def __ :tests-will-fail)
+(defn this-dedupe [a]
+ (map first (partition-by identity a)))
 
 (comment
-  
+  (def a [1 1 2 3 3 3 2 2 3])
+  (->> a
+       (partition-by identity ,,,)
+       (map first ,,,)
+       ) 
+
   )
 
 (tests
-  (apply str (__ "Leeeeeerrroyyy")) := "Leroy"
-  (__ [1 1 2 3 3 2 2 3]) := '(1 2 3 2 3)
-  (__ [[1 2] [1 2] [3 4] [1 2]]) := '([1 2] [3 4] [1 2]))
+  (apply str (this-dedupe "Leeeeeerrroyyy")) := "Leroy"
+  (this-dedupe [1 1 2 3 3 2 2 3]) := '(1 2 3 2 3)
+  (this-dedupe [[1 2] [1 2] [3 4] [1 2]]) := '([1 2] [3 4] [1 2]))
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/92a4bd13aaa6bffb80d7724de2c8e64d

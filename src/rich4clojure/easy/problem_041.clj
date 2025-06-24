12fedb73c66c6c58417b7f2a80c68bdf48457487
Faith Rotich <rotichfaith43@gmail.com>
2025-06-25 00:52:00 +0100

    solved 39 - drop nth

--- src/rich4clojure/easy/problem_041.clj
+++ src/rich4clojure/easy/problem_041.clj
@@ -9,16 +9,44 @@
 ;; Write a function which drops every Nth item from a
 ;; sequence.
 
-(def __ :tests-will-fail)
-
-(comment
-  
+(defn drop-nth [coll n]
+   (keep-indexed (fn [index item]
+                  (when (not (= (dec n) (mod index n)))
+                    item))
+                coll)
   )
 
+(comment
+  drop
+  (#(drop %2 %1) [1 2 3 4 5] 2)
+
+  (take 3 [1 2 3 4 5 6 7 8])
+  (every?)
+
+  (keep (fn [n] (when (odd? n) (* 2 n))) [1 2 3 4 5])
+
+  map
+  map-indexed
+  filter
+  keep
+  keep-indexed
+
+  (keep-indexed (fn [index item]
+                  (when (not (= (dec 3) (mod index 3)))
+                    item))
+                [1 2 3 4 5 6 7 8])
+  
+  (zero? (mod 10 3))
+  
+
+  )
+
+(= [1 2 3] '(1 2 3))
+
 (tests
-  (__ [1 2 3 4 5 6 7 8] 3) := [1 2 4 5 7 8]
-  (__ [:a :b :c :d :e :f] 2) := [:a :c :e]
-  (__ [1 2 3 4 5 6] 4) := [1 2 3 5 6])
+  (drop-nth [1 2 3 4 5 6 7 8] 3) := [1 2 4 5 7 8]
+  (drop-nth [:a :b :c :d :e :f] 2) := [:a :c :e]
+  (drop-nth [1 2 3 4 5 6] 4) := [1 2 3 5 6])
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/03788b118a3d7923f7aae143e8ef1aee

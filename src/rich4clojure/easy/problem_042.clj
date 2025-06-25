a16bb783d9e72a5dadf46eed910e0ad0366ef551
Faith Rotich <rotichfaith43@gmail.com>
2025-06-25 01:23:09 +0100

    solved 42 - factorials

--- src/rich4clojure/easy/problem_042.clj
+++ src/rich4clojure/easy/problem_042.clj
@@ -8,17 +8,37 @@
 ;; 
 ;; Write a function which calculates factorials.
 
-(def __ :tests-will-fail)
+(defn factorial [n]
+  (apply * (range 1 (inc n))))
 
 (comment
+  ; 1 
+  ; 2 x 1
+  ; 3 x 2 x 1
+  ; 4 x 3 x 2 x 1
+  ; 5 x 4 x 3 x 2 x 1
   
+  (apply * (range 1 (inc 8)))
+
+  (loop [n     5
+         total 1]
+    
+    (if (= n 1)
+      total
+      
+      (recur (dec n) (* total n))))
+  
+  (let [a 1
+        b :foo]
+    [a a b b])
+
   )
 
 (tests
-  (__ 1) := 1
-  (__ 3) := 6
-  (__ 5) := 120
-  (__ 8) := 40320)
+  (factorial 1) := 1
+  (factorial 3) := 6
+  (factorial 5) := 120
+  (factorial 8) := 40320)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/850039024c0d503cce2b2e98ca1803c5

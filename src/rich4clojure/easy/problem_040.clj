e78b8757d48a6d7da07cd1655f0e6c6e681eed2b
Faith Rotich <rotichfaith43@gmail.com>
2025-06-27 00:52:22 +0100

    solved 40 - separate collection

--- src/rich4clojure/easy/problem_040.clj
+++ src/rich4clojure/easy/problem_040.clj
@@ -11,16 +11,23 @@
 
 (def restricted [interpose])
 
-(def __ :tests-will-fail)
+(defn sep-coll [x coll]
+  (rest (interleave (repeat (count coll) x) coll))
+  )
+
 
 (comment
-  
+  (interleave (repeat (count [1 2 3]) :a) [1 2 3])
+  (seq)
+  (repeat 3 :a)
+  (count [1 2 3])
+  (rest)
   )
 
 (tests
-  (__ 0 [1 2 3]) := [1 0 2 0 3]
-  (apply str (__ ", " ["one" "two" "three"])) := "one, two, three"
-  (__ :z [:a :b :c :d]) := [:a :z :b :z :c :z :d])
+  (sep-coll 0 [1 2 3]) := [1 0 2 0 3]
+  (apply str (sep-coll ", " ["one" "two" "three"])) := "one, two, three"
+  (sep-coll :z [:a :b :c :d]) := [:a :z :b :z :c :z :d])
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/4f5ffbf020d90a968db4f99478cf358e
